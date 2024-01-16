2aef6e264777320f6df8c8b168a3d6e380fea102
Habibasubhani <137423313+Habibasubhani@users.noreply.github.com>
2024-01-17 00:06:09 +0530

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we are using nodejs not browser
+console.log(2+2); console.log("hitesh") // code readability down not preferred
+let name = "habiba"
+let age = 20
+let isLoggedIn = false
+let state;
+///////////////DATA TYPES////////////
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/flase
+//null => standalone value, value abhi ayi hi nhi hai
+//undefined => value not assign
+//symbol => uniqueness
+console.log(typeof name)
+console.log(typeof null)//null is an OBJECT
+console.log(typeof undefined) //undefined apne aap me ek TYPE *hai
